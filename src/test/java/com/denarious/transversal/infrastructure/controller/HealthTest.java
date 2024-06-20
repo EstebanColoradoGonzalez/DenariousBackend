a7c2926574d7af86027ce81ec5b9f7eca7dc92e1
Esteban <estebancoloradogonzalez@gmail.com>
2024-06-19 19:24:57 -0500

    test: Add unit test to improve coverage
    Added a new unit test to increase the overall test coverage. This test targets functionality in the user subdomain to ensure comprehensive coverage and reliability.

--- src/test/java/com/denarious/transversal/infrastructure/controller/HealthTest.java
+++ src/test/java/com/denarious/transversal/infrastructure/controller/HealthTest.java
@@ -1,11 +1,16 @@
 package com.denarious.transversal.infrastructure.controller;
 
+import com.denarious.transversal.domain.constants.HealthConstant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import static com.denarious.transversal.domain.constants.HealthConstant.OK_STATUS;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @WebFluxTest(controllers = Health.class)
 class HealthTest {
@@ -20,5 +25,18 @@ class HealthTest {
                 // Assert
                 .expectStatus().isOk()
                 .expectBody(String.class).isEqualTo(OK_STATUS);
+    }
+
+    @Test
+    public void testPrivateConstructor() throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        // Arrange
+        Constructor<HealthConstant> constructor = HealthConstant.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+
+        // Act
+        HealthConstant instance = constructor.newInstance();
+
+        // Assert
+        assertNotNull(instance);
     }
 }
